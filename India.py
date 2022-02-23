91927705547e90277e0fc09760145c8363ca8c0c
HANNAN ANSARI <90706836+HANNAN0098@users.noreply.github.com>
2022-02-23 11:10:08 +0500

    Update India.py

--- India.py
+++ India.py
@@ -147,8 +147,6 @@ def Zeek():
     os.system('clear')
     print logo1
     print '\033[1;93m[\033[1;91m1\033[1;93m]\033[1;94m START CLONING'   
-    print '\033[1;91mWAIT KAR JANI.....😁 '
-    time.sleep.(5.0)
     okayy()
  
  def okayy():
